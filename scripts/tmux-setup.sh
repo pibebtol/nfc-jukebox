841439f073513fa80c17834ef09d19fc867c5717
pibebtol <julius.felchow@gmail.com>
2025-01-02 21:41:28 +0100

    small fixes in the bootup script paths

--- scripts/tmux-setup.sh
+++ scripts/tmux-setup.sh
@@ -6,10 +6,10 @@ echo "[juke-the-box.sh] start new tmux session with the jukebox and spotify"
 tmux new-session -d -s nfc-jukebox
 
 # Create the first window and run spotify_player
-tmux send-keys -t nfc-jukebox:1 '~/workspace/nfc-jukebox/spotify-starter.sh' C-m
+tmux send-keys -t nfc-jukebox:1 '~/workspace/nfc-jukebox/scripts/spotify-starter.sh' C-m
 
 # Create a second window and run the nfc-jukebox script
 tmux new-window -t nfc-jukebox:2 -n
-tmux send-keys -t nfc-jukebox:2 '~/workspace/nfc-jukebox/target/release/nfc-jukebox' C-m
+tmux send-keys -t nfc-jukebox:2 'cd ~/workspace/nfc-jukebox; ./target/release/nfc-jukebox' C-m
 
 echo "[juke-the-box.sh] created new tmux session"
